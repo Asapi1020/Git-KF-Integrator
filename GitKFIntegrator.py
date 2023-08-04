77d8d92bcfaccb5961a56942d2b1aa9d2bf88f85
asapi1020 <oneway12150329@gmail.com>
2023-08-04 21:50:53 +0900

    Checking settings and KFEditor.ini

--- GitKFIntegrator.py
+++ GitKFIntegrator.py
@@ -12,25 +12,71 @@ home_dir = "C:\\Users\\maima\\Documents\\My Games\\KillingFloor2"
 kfeditor_cfg = home_dir + "\\KFGame\\Config\\KFEditor.ini"
 log_dir = home_dir + "\\KFGame\\Logs"
 
+mod_packages_in_path = "..\\..\\KFGame\\GitSrc\\CD-Combined-Edition"
+mod_output_dir = "..\\..\\KFGame\\Unpublished\\BrewedPC\\Script"
+mod_packages = ["CustomHUD", "CombinedCD2", "CombinedCDContent"]
+
 class MyHandler(FileSystemEventHandler):
 	log_mod_count = 0
 
 	def on_modified(self, event):
 		file_name = event.src_path.replace((log_dir + "\\"), "")
-		print("[Modified]", file_name)
+		print("[Modified]", file_name, sep="\t")
 		if file_name == "Launch.log":
 			self.log_mod_count += 1
 
 	def on_created(self, event):
 		file_name = event.src_path.replace((log_dir + "\\"), "")
-		print("[Created]", file_name)
+		print("[Created]", file_name, sep="\t")
+
+def get_mod_packages_section():
+	line = "[ModPackages]\n"
+	line += "ModPackagesInPath=" + mod_packages_in_path + "\n"
+	line += "ModOutputDir=" + mod_output_dir + "\n"
+	for i in range(len(mod_packages)):
+		line += "ModPackages=" + mod_packages[i] + "\n"
+	print("\n" + line)
+	return line
+
+def setup_editor_cfg():
+	# Reading
+	with open(kfeditor_cfg, 'r') as file:
+		lines = file.readlines()
+
+	# Modifying
+	modified_lines = []
+	section_stage = 0
+
+	for line in lines:
+		match(section_stage):
+			# seeking, always append sth
+			case 0:
+				if line == "[ModPackages]\n":
+					line = get_mod_packages_section()
+					section_stage = 1
+				modified_lines.append(line)
+			# skipping to the next section
+			case 1:
+				if "[" in line:
+					section_stage = 2
+					modified_lines.append(line)
+			# This case should stand for just appending left lines.
+			case _:
+				modified_lines.append(line)
+	if section_stage == 0:
+		modified_lines.append(get_mod_packages_section())
+
+	# Writing
+	with open(kfeditor_cfg, 'w') as file:
+		file.writelines(modified_lines)
 
 if __name__ == "__main__":
 	try:
 		# init
 		logging.basicConfig(level=logging.INFO,
-    						format="[Log] %(message)s")
+    						format="[Log]\t%(message)s")
 		subprocess.run("git --version")
+		setup_editor_cfg()
 
 		# compile with tracking
 		event_handler = MyHandler() #LoggingEventHandler()
@@ -50,5 +96,5 @@ if __name__ == "__main__":
 			observer.join()
 
 	except:
-		print("ERROR!")
+		print("[ERROR]\tFatal Error!")
 		sys.exit()
